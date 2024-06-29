f2f7f8e0712683643999e41588eab678fb9c366c
Hoang Vuong Vu <hoangvuong.v99@gmail.com>
2024-06-29 17:29:48 +0200

    refactor: Update collection name in migration script

---
+++ pb_migrations/1719674958_updated_posts.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("b742m2521w33ndz")
+
+  collection.name = "notes"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("b742m2521w33ndz")
+
+  collection.name = "posts"
+
+  return dao.saveCollection(collection)
+})
